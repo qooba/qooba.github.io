d94401e7279894dbd1b6c4e677e2f1b4b0885725
qooba <dev@qooba.net>
2022-05-03 12:20:40 +0200

    add cookie consent

--- _includes/gtag.js
+++ _includes/gtag.js
@@ -1,9 +1,4 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-M934D3FRMF"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-M934D3FRMF');
-</script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-M934D3FRMF');
